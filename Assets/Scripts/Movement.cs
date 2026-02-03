f89bed414dc36f19f68491c5e021661f8006dcec
GhareebXenon <108562554+GhareebXenon@users.noreply.github.com>
2026-02-03 04:36:05 +0200

    Tetris-Grid-Upgrade

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,48 +5,69 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour {
 	public float timestep = 0.2F; 
-	float time; 
-
-	//The actual group which can rotate and will move down
-	public GameObject actualGroup; 
+	float time;
+ 
+    //The actual group which can rotate and will move down
+    public GameObject actualGroup; 
 
 	public void startGame(){
 		actualGroup = this.gameObject.GetComponent<GroupSpawner> ().spawnNext ();
 	}
-	//Move down in interval of timestep
-	void Update () {
-		time += Time.deltaTime; 
-		if (time > timestep) {
-			time = 0; 
-			if (actualGroup != null) {
-				move (Vector3.down); 
-			}
-		}
-		checkForInput (); 
-	}
+    //Move down in interval of timestep
+    void Update()
+    {
+        if (actualGroup == null)
+            return; //  do nothing until a group exists
 
-	void checkForInput(){
-		if (Input.GetKeyDown (KeyCode.R)) {
-			actualGroup.GetComponent<Rotation>().rotateRight (); 
-		} else if (Input.GetKeyDown (KeyCode.L)) {
-			actualGroup.GetComponent<Rotation>().rotateLeft (); 
-		}
-		if (Input.GetKeyDown (KeyCode.A)) {
-			move (Vector3.left);
-		} else if (Input.GetKeyDown (KeyCode.D)) {
-			move (Vector3.right);
-		}
-		if (Input.GetKey (KeyCode.S)) {
-			timestep = 0.05F; 
-		} else {
-			setNewSpeed(); 
-		}
-		gameObject.GetComponent<CubeArray> ().getCubePositionFromScene ();
-	}
+        time += Time.deltaTime;
+        if (time > timestep)
+        {
+            time = 0;
+            move(Vector3.down);
+        }
+
+        checkForInput();
+    }
 
 
-	//Speed increasement found at http://www.colinfahey.com/tetris/tetris.html 5.10 
-	public void setNewSpeed(){
+    void checkForInput()
+    {
+        if (actualGroup == null) return;
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            actualGroup.GetComponent<Rotation>().rotateRight();
+        }
+        else if (Input.GetKeyDown(KeyCode.L))
+        {
+            actualGroup.GetComponent<Rotation>().rotateLeft();
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            move(Vector3.left);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            move(Vector3.right);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            timestep = 0.05F;
+        }
+        else
+        {
+            setNewSpeed();
+        }
+
+        GetComponent<CubeArray>().getCubePositionFromScene();
+    }
+
+
+
+    //Speed increasement found at http://www.colinfahey.com/tetris/tetris.html 5.10 
+    public void setNewSpeed(){
 		timestep = ((10 - gameObject.GetComponent<Highscore> ().level) * 0.05F);
 	}
 
